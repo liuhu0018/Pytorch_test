315a88668a93fd77b712943ea07922723f46b473
liuhu0018 <80232960@qq.com>
2020-09-09 23:45:04 +0800

    add

---
+++ LSTM.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import torch
+import matplotlib.pyplot as plt
+import torch.utils.data as Data
+
+data = pd.read_excel('C:\\Users\\80232\\Documents\\data\\data.xlsx')
+BATCH_SIZE = 25
+x_ = pd.DataFrame(data, columns=['X', 'Y']).values
+x = torch.FloatTensor(x_)
+x = torch.autograd.Variable(x, requires_grad=False)
+# print(x.shape)
+y_ = pd.DataFrame(data, columns=['pwm1', 'pwm2']).values
+y = torch.FloatTensor(y_)
+y = torch.autograd.Variable(y, requires_grad=False)
+x_train = x[:-2]
+y_train = y[:-2]
+x_test = x[-6:]
+y_test = y[-6:]
+print(x_train)
+print(y_train)
+print(x_test)
+print(y_test)
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+dataset = Data.TensorDataset(x_train, y_train)
+load = Data.DataLoader(dataset=dataset,
+                       batch_size=BATCH_SIZE,
+                       shuffle=True)
+model = torch.nn.Sequential(
+    # torch.nn.BatchNorm1d(2),
+    torch.nn.LSTM(2, 2),
+    # # torch.nn.BatchNorm1d(48),
+    # torch.nn.ReLU(),
+    # torch.nn.Linear(16, 8),
+    # torch.nn.ReLU(),
+    # # torch.nn.Linear(500, 8),
+    # # torch.nn.ReLU(),
+    # torch.nn.Linear(8, 2),
+    # # torch.nn.BatchNorm1d(2)
+)
+# print(model)
+# model.to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+loss_func = torch.nn.MSELoss()
+epoch_list = []
+loss_list = []
+
+for epoch in range(2000):
+    for step, (x_train, y_train) in enumerate(load):
+        # x_train = x_train.to(device)
+        # y_train = y_train.to(device)
+        y_pred = model(x_train)
+        loss = loss_func(y_pred, y_train)
+        epoch_list.append(epoch)
+        loss_list.append(loss)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print("Epoch:{}, Loss:{:.4f}".format(epoch, loss.data.item()))
+
+print(model(x_test))
+plt.plot(epoch_list, loss_list)
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.grid()
+plt.show()
+
+
+# torch.save(model.state_dict(), 'model.pkl')
+# torch.save(model.state_dict(), 'net_param.pkl')
